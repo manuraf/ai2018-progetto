536551a75905fdea60a5854a1f5e586f35ee162d
MANUEL <manuel@MBP-di-MANUEL.homenet.telecomitalia.it>
2018-09-06 20:56:32 +0200

    aggiunta attributi model

--- src/app/archivi/archivio.model.ts
+++ src/app/archivi/archivio.model.ts
@@ -1,3 +1,7 @@
 export class Archivio {
-    
+    id: number;
+    rappr_timestamp: string;
+	rappr_posizioni: string;
+    acquistabile: boolean;
+    //posizioni
 }
--- src/app/posizioni/posizione.model.ts
+++ src/app/posizioni/posizione.model.ts
@@ -1,3 +1,7 @@
 export class Posizione {
+    timestamp: Date;
+    id: number;
+    location: string;
+
     
 }
